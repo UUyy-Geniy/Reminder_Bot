7e7c15e657660b44801fb0be4ae680e9762257e8
UUyy-Geniy <jekapeheka@gmail.com>
2024-05-21 21:30:30 +0300

    add migrations to build

--- bd/db.py
+++ bd/db.py
@@ -1,9 +1,9 @@
-import logging
 import os
+import logging
+import subprocess  # Импорт модуля subprocess
 from dotenv import load_dotenv
-from sqlalchemy import create_engine, select, update, delete, Engine, literal_column, join
+from sqlalchemy import create_engine, select
 from sqlalchemy.orm import sessionmaker
-
 
 class Database:
     def __init__(self, db_url):
@@ -12,15 +12,23 @@ class Database:
         self.engine = None
 
     def connect(self):
-
         try:
             self.engine = create_engine(self.url)
             self.session_maker = sessionmaker(bind=self.engine)
             self.sql_query(query=select(1))
             logging.info("Database connected")
+            self.run_migrations() # Вызов метода для запуска миграций
         except Exception as e:
             logging.error(e)
             logging.error("Database didn't connect")
+
+    def run_migrations(self):
+        # Функция для запуска Alembic миграций
+        try:
+            subprocess.run(["alembic", "upgrade", "head"], check=True)
+            logging.info("Migrations applied successfully")
+        except subprocess.CalledProcessError as e:
+            logging.error(f"Failed to apply migrations: {e}")
 
     def sql_query(self, query, is_single=True, is_update=False, is_delete=False):
         with self.session_maker(expire_on_commit=True) as session:
@@ -29,18 +37,6 @@ class Database:
                 session.commit()
             if not is_update and not is_delete:
                 return response.scalars().first() if is_single else response.all()
-
-    def create_object(self, model, ):
-        with self.session_maker(expire_on_commit=True) as session:
-            session.add(model)
-            session.commit()
-            session.refresh(model)
-            return model.id
-
-    def create_objects(self, model_s: []):
-        with self.session_maker(expire_on_commit=True) as session:
-            session.add_all(model_s)
-            session.commit()
 
 load_dotenv()
 logging.basicConfig(level=logging.INFO)
